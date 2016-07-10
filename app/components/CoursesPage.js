bf7ea99eac66e6d465cfa8e487bc0017978d03a8
Jaume <Jaume>
2016-07-10 16:43:02 +0200

    Remove Inline Form

--- app/components/CoursesPage.js
+++ app/components/CoursesPage.js
@@ -9,16 +9,6 @@ class CoursesPage extends React.Component {
         this.state = {
             course: {title: ''}
         };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-    onTitleChange (event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-    onClickSave () {
-        this.props.actions.createCourse(this.state.course);
     }
     courseRow (course, index) {
         return <div key={index}>{course.title}</div>;
@@ -28,17 +18,6 @@ class CoursesPage extends React.Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title}
-                />
-                <input
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave}
-                />
             </div>
         );
     }
